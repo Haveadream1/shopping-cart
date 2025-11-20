926c6286e43507d0f6464dd608e1331009519c57
Haveadream1 <260602david@gmail.com>
2025-11-20 17:28:34 +0100

    feat: display selected items

--- src/containers/CartContainer.jsx
+++ src/containers/CartContainer.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import CartItem from "../components/CartItem";
 import Header from "../components/Header";
+import { useCart } from "./CartContext";
+import { useData } from "./DataContext";
 
 export default function Cart() {
+    const dataArr = useData();
+    const {idArr, isItemIdInCart} = useCart();
+
+    const [selectionedArr, setSelectionedArr] = useState([]);
+
+    const retrieveSelectionedItems = (arr) => {
+        const selectionedItems = [];
+
+        // loop to retrieve the item from the fetched data with the selected id
+        for (let i = 0; i < arr.length; i++) {
+            if (isItemIdInCart(arr[i].id)) {
+                selectionedItems.push(arr[i]);
+            }
+        }
+        setSelectionedArr(selectionedItems);
+    }
+
+    useEffect(() => {
+        retrieveSelectionedItems(dataArr);
+    }, []);
+
     return (
         <>
             <Header className={"header-cart-page"} />
@@ -14,71 +39,19 @@ export default function Cart() {
                     </span>
 
                     <div className="items-grid">
-                        <div className="cart-item">
-                            <img id="cart-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
-
-                            <div className="item-content">
-                                <p className="item-name">Clasic White T-shirt</p>
-                                <p className="item-price">25 000₩</p>
-                            </div>
-
-                            <div className="item-related-buttons">
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Increase icon" alt="Increase icon" loading="lazy">add</span>
-                                </button>
-                                <p className="quantity-text">1</p>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Decrease icon" alt="Decrease icon" loading="lazy">remove</span>
-                                </button>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Remove icon" alt="Remove icon" loading="lazy">delete</span>
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="cart-item">
-                            <img id="cart-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
-
-                            <div className="item-content">
-                                <p className="item-name">Clasic White T-shirt</p>
-                                <p className="item-price">25 000₩</p>
-                            </div>
-
-                            <div className="item-related-buttons">
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Increase icon" alt="Increase icon" loading="lazy">add</span>
-                                </button>
-                                <p className="quantity-text">1</p>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Decrease icon" alt="Decrease icon" loading="lazy">remove</span>
-                                </button>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Remove icon" alt="Remove icon" loading="lazy">delete</span>
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="cart-item">
-                            <img id="cart-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
-
-                            <div className="item-content">
-                                <p className="item-name">Clasic White T-shirt</p>
-                                <p className="item-price">25 000₩</p>
-                            </div>
-
-                            <div className="item-related-buttons">
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Increase icon" alt="Increase icon" loading="lazy">add</span>
-                                </button>
-                                <p className="quantity-text">1</p>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Decrease icon" alt="Decrease icon" loading="lazy">remove</span>
-                                </button>
-                                <button className="item-button" type="button">
-                                    <span className="material-symbols-outlined" role="img" aria-label="Remove icon" alt="Remove icon" loading="lazy">delete</span>
-                                </button>
-                            </div>
-                        </div>
+                        {selectionedArr ? (
+                            selectionedArr.map((item, index) => 
+                                <CartItem 
+                                    key={index}
+                                    imageSrc={item.image}
+                                    imageAlt={item.title}
+                                    imageName={item.title}
+                                    itemPrice={item.price}
+                                />
+                            )
+                        ) : (
+                            <p>Loading ...</p>
+                        )}
                     </div>
                 </section>
 
@@ -104,3 +77,26 @@ export default function Cart() {
         </>
     );
 }
+
+
+{/* <div className="cart-item">
+                            <img id="cart-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
+
+                            <div className="item-content">
+                                <p className="item-name">Clasic White T-shirt</p>
+                                <p className="item-price">25 000₩</p>
+                            </div>
+
+                            <div className="item-related-buttons">
+                                <button className="item-button" type="button">
+                                    <span className="material-symbols-outlined" role="img" aria-label="Increase icon" alt="Increase icon" loading="lazy">add</span>
+                                </button>
+                                <p className="quantity-text">1</p>
+                                <button className="item-button" type="button">
+                                    <span className="material-symbols-outlined" role="img" aria-label="Decrease icon" alt="Decrease icon" loading="lazy">remove</span>
+                                </button>
+                                <button className="item-button" type="button">
+                                    <span className="material-symbols-outlined" role="img" aria-label="Remove icon" alt="Remove icon" loading="lazy">delete</span>
+                                </button>
+                            </div>
+                        </div> */}
