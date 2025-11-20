a136514b759168760854f9b5441ad4395b36edb0
Haveadream1 <260602david@gmail.com>
2025-11-20 17:28:04 +0100

    feat: handle button click with id

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,15 +1,25 @@
+import { useCart } from "../containers/CartContext";
+
 export default function ShopItem({ 
+    itemId,
     imageSrc,
     imageAlt,
     itemName,
-    itemPrice
+    itemPrice,
 }) {
+    const {addItemsIdToCart, idArr} = useCart();
+
+    const handleButtonClick = () => {
+        addItemsIdToCart(itemId)
+        console.log(idArr)
+    };
+
     return (
         <div className="shop-item">
             <img className="shop-item-image" src={imageSrc} alt={imageAlt} loading="lazy" />
             <p className="shop-item-name">{itemName}</p>
             <p className="shop-item-price">{itemPrice} ₩</p>
-            <button className="add-to-cart-button">Add to cart</button>
+            <button className="add-to-cart-button" onClick={handleButtonClick}>Add to cart</button>
         </div>
     );
 }
