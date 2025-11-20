3821d02eb39c4995337dc29523cc57aca747ce3c
Haveadream1 <260602david@gmail.com>
2025-11-20 17:28:48 +0100

    feat: init cart context for data

---
+++ src/containers/CartContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export default function CartProvider({ children }) {
+    const [idArr, setIdArr] = useState([]);
+
+    const addItemsIdToCart = (itemId) => {
+        setIdArr(prev => {
+            return [...prev, itemId];
+        })
+    }
+
+    const isItemIdInCart = (itemId) => {
+        return idArr.includes(itemId)
+    }
+
+    const value = {
+        idArr,
+        addItemsIdToCart,
+        isItemIdInCart
+    }
+
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export const useCart = () => {
+    const context = useContext(CartContext);
+    if (!context) {
+        throw new Error("useData must be used in a DataProvider");
+    }
+    return context;
+}
