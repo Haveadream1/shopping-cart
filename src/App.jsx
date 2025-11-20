149d294d5b002d3f659da7bfcae6b4d8c8d4b83f
Haveadream1 <260602david@gmail.com>
2025-11-20 17:27:11 +0100

    remove: old commented code

--- src/App.jsx
+++ src/App.jsx
@@ -6,52 +6,9 @@ import Home from './containers/HomeContainer';
 import Shop from './containers/ShopContainer';
 
 export default function App() {
-
-    // const location = useLocation();
-
-    // const setRootClass = () => {
-    //     switch (location.pathname) {
-    //         case "/":
-    //             return "body-home-page";
-    //         case "/shop":
-    //             return "body-shop-page";
-    //         case "/cart":
-    //             return "body-cart-page";
-    //         default:
-    //             return "body-home-page";
-    //     }
-    // }
-
     return (
         <>
             <Header className={"header-home-page"} isHomePageSelected={true}/>
         </>       
     );
 }
-
-// export default function App() {
-//     return (
-//         <>
-//             <Header className={"header-home-page"} isHomePageSelected={true}/>
-//             <Home />
-//         </>
-//     );
-// }
-
-// export default function App() {
-//     return (
-//         <>
-//             <Header />
-//             <Shop />
-//         </>
-//     );
-// }
-
-// export default function App() {
-//     return (
-//         <>
-//             <Header />
-//             <Cart />
-//         </>
-//     );
-// }
