eecd28a64cf033485ec187344f7784b39309c2d5
Haveadream1 <260602david@gmail.com>
2025-11-21 11:29:54 +0100

    feat: handle button click

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import { useCart } from "../containers/CartContext";
+
 export default function CartItem({
+    itemId,
     imageSrc,
     imageAlt,
     itemName,
     itemPrice,
 }) {
+    // const [itemQuantity, setItemQuantity] = useState(1);
+    const {removeIdFromCart, increaseItemQuantity, decreaseItemQuantity, itemQuantity} = useCart();
+
+    const handleIncreaseButton = () => {
+        increaseItemQuantity(itemId)
+        // setItemQuantity(prev => prev + 1);
+    }
+    const handleDecreaseButton = () => {
+        decreaseItemQuantity(itemId)
+        // if (itemQuantity > 1 ) {
+        //     setItemQuantity(prev => prev - 1);
+        // }
+    }
+    const handleDeleteButton = () => {
+        removeIdFromCart(itemId);
+        // setItemQuantity(1); // TODO need to fix issue
+    }
+
+    useEffect(() => {
+        console.log(itemQuantity[itemId]);
+    }, [itemQuantity]);
+
     return (
         <div className="cart-item">
             <img id="cart-item-image" src={imageSrc} alt={imageAlt} loading="lazy" />
@@ -14,14 +40,21 @@ export default function CartItem({
             </div>
 
             <div className="item-related-buttons">
-                <button className="item-button" type="button">
+                <button className="item-button" type="button" onClick={handleIncreaseButton}>
                     <span className="material-symbols-outlined" role="img" aria-label="Increase icon" alt="Increase icon" loading="lazy">add</span>
                 </button>
-                <p className="quantity-text">1</p>
-                <button className="item-button" type="button">
+                <p className="quantity-text">
+                    {/* {itemQuantity[itemId] ? (
+                        `${itemQuantity[itemId]}`
+                    ) : (
+                        <p>loading...</p>
+                    )} */}
+                    {itemQuantity[itemId]}
+                </p>
+                <button className="item-button" type="button" onClick={handleDecreaseButton}>
                     <span className="material-symbols-outlined" role="img" aria-label="Decrease icon" alt="Decrease icon" loading="lazy">remove</span>
                 </button>
-                <button className="item-button" type="button">
+                <button className="item-button" type="button" onClick={handleDeleteButton}>
                     <span className="material-symbols-outlined" role="img" aria-label="Remove icon" alt="Remove icon" loading="lazy">delete</span>
                 </button>
             </div>
