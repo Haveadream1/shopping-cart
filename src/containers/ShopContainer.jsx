632ae362c8f65ff4552b47f8b2ad114b77ca27f6
Haveadream1 <260602david@gmail.com>
2025-11-22 14:59:17 +0100

    feat: remove old comments

--- src/containers/ShopContainer.jsx
+++ src/containers/ShopContainer.jsx
@@ -45,17 +45,3 @@ export default function Shop() {
         </>
     );
 }
-
-                    // <div className="shop-item">
-                    //     <img className="shop-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
-                    //     <p className="shop-item-name">Ergo comfort chair</p>
-                    //     <p className="shop-item-price">36 000₩</p>
-                    //     <button className="add-to-cart-button">Add to cart</button>
-                    // </div>
-
-                    // <div className="shop-item">
-                    //     <img className="shop-item-image" src="https://images.pexels.com/photos/6694337/pexels-photo-6694337.jpeg" alt="" loading="lazy" />
-                    //     <p className="shop-item-name">Ergo comfort chair</p>
-                    //     <p className="shop-item-price">36 000₩</p>
-                    //     <button className="add-to-cart-button">Add to cart</button>
-                    // </div>
