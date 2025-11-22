1b3693f211e20468005afbab92c620e7cf681fc0
Haveadream1 <260602david@gmail.com>
2025-11-22 15:00:18 +0100

    feat: remove unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 import CartProvider from './containers/CartContext.jsx'
 import Home from './containers/HomeContainer.jsx'
