6764660e5c808fa693fbf36326c8d6993f711512
Haveadream1 <260602david@gmail.com>
2025-11-22 15:56:54 +0100

    feat: init header test

---
+++ src/tests/Header.test.jsx
@@ -0,0 +1,8 @@
+const { renderMatches } = require("react-router");
+const { describe, it } = require("vitest");
+
+describe("Header component", () => {
+    it("renders correct heading", () => {
+        render()
+    })
+})
